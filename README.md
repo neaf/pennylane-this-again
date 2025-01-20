2646fb564dec4604a62e4c5c22e3946c0bd77344
Tomasz Werbicki <tomasz@werbicki.net>
2025-01-20 15:06:02 +0100

    Final touches and cleanup

--- README.md
+++ README.md
@@ -1,24 +1,15 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Hello Pennylane people!
 
-Things you may want to cover:
+My first thought after reading the task description was the following:
 
-* Ruby version
+The tool is so not critical that no one is going to endure any amount of friction. People want to sloppily type or dictate stuff and expect us to make it work. Hence I, opted for super simple input interface, even if that means sacrificing results "accuracy".
 
-* System dependencies
+Using trigram similarity might match "olives" with "olive oil" or miss "rice" in "1 (8.5 ounce) package UNCLE BEN'S® Jasmine READY RICE®". However, assuming no recipe is going to exactly match the user's stock and people will mostly use the app for inspiration, I tried to select a treshold that strucks the right balance between input forgiveness and correctness of results.
 
-* Configuration
+It was a fun little challenge, even more so because I received it right after I promised my girlfriend to prepare a weekly meal plan, so we can avoid decision fatigue every other day. :)
 
-* Database creation
+I hope you enjoying playing with the solution.
 
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+# Running
---
+++ spec/services/recipes/searcher_spec.rb
@@ -0,0 +1,159 @@
+require "rails_helper"
+
+RSpec.describe Recipes::Searcher do
+  subject(:searcher) do
+    described_class.new(
+      ingredients: search_ingredients,
+    )
+  end
+
+  let!(:recipes) do
+    [
+      create(:recipe, ingredients: [
+        "2 cups uncooked glutinous white rice",
+        "4 cups water",
+        "6 eggs, beaten",
+        "6 sheets nori (dry seaweed)",
+        "¼ cup basil pesto",
+        "salt and pepper to taste",
+      ]),
+      create(:recipe, ingredients: [
+        "1 pound skinless, boneless chicken breasts",
+        "½ cup Italian-style salad dressing, or to taste",
+        "3 ounces Brie cheese, sliced",
+        "1 tablespoon olive oil, divided",
+        "2 cups cherry tomatoes, halved",
+        "¼ teaspoon kosher salt, divided",
+        "3 tablespoons balsamic vinegar, divided",
+        "1 tablespoon chopped fresh basil",
+        "2 cups fresh spinach leaves",
+        "¼ cup mayonnaise",
+        "1 tablespoon Dijon mustard",
+        "2 cloves garlic, minced",
+        "⅛ teaspoon ground black pepper",
+        "1 loaf French bread, halved lengthwise",
+      ]),
+      create(:recipe, ingredients: [
+        "1 cup tightly packed chopped parsley leaves",
+        "1 cup tightly packed chopped cilantro leaves",
+        "¼ cup red wine vinegar",
+        "½ onion, coarsely chopped",
+        "5 cloves garlic",
+        "1 teaspoon coarse salt",
+        "1 teaspoon dried oregano",
+        "1 teaspoon hot pepper flakes",
+        "1 teaspoon freshly ground black pepper",
+        "½ cup extra-virgin olive oil",
+      ]),
+      create(:recipe, ingredients: [
+        "1 ⅓ cups brown sugar",
+        "2 tablespoons ground cinnamon",
+        "1 tablespoon butter",
+        "2 teaspoons honey",
+        "8 small Golden Delicious apples, peeled and cut into small pieces",
+        "1 refrigerated pie crust",
+      ]),
+      create(:recipe, ingredients: [
+        "1 (15 ounce) package beef roast in au jus",
+        "1 (8.5 ounce) package UNCLE BEN'S® Jasmine READY RICE®",
+        "1 tablespoon vegetable oil, divided",
+        "1 (8 ounce) package button mushrooms, quartered",
+        "1 teaspoon Korean barbecue sauce",
+        "4 cups baby spinach",
+        "1 clove garlic, finely chopped",
+        "2 cups shredded carrots",
+        "1 teaspoon chopped fresh ginger root",
+        "2 tablespoons Korean barbecue sauce, or to taste",
+      ]),
+    ]
+  end
+
+  describe "#recipes" do
+    describe "empty search ingredients behaviour" do
+      let(:search_ingredients) do
+        []
+      end
+
+      it "returns no recipes" do
+        expect(searcher.recipes).to eq([])
+      end
+    end
+
+    describe "rejecting recipes without matching ingredients" do
+      let(:search_ingredients) do
+        [
+          "vinegar",
+          "garlic",
+          "spinach"
+        ]
+      end
+
+      it "returns only matching recipes" do
+        expect(searcher.recipes).to match_array(recipes.values_at(
+          1, 2, 4
+        ))
+      end
+    end
+
+    describe "promoting recipes with most matching ingredients" do
+      let(:search_ingredients) do
+        [
+          "vinegar",
+          "garlic",
+          "spinach",
+          "beef",
+          "carrots",
+        ]
+      end
+
+      it "orders by amount of matching ingredients" do
+        expect(searcher.recipes).to eq(recipes.values_at(
+          4, 1, 2
+        ))
+      end
+    end
+
+    describe "demoting recipes with most non-matching ingredients" do
+      let(:search_ingredients) do
+        [
+          "spinach",
+          "garlic",
+        ]
+      end
+
+      it "orders by amount of matching ingredients" do
+        expect(searcher.recipes).to eq(recipes.values_at(
+          4, 1, 2
+        ))
+      end
+    end
+
+    describe "fuzzy matching ingredients" do
+      let(:search_ingredients) do
+        [
+          "mayonnase",
+        ]
+      end
+
+      it "returns recipes with mistyped search ingredients" do
+        expect(searcher.recipes).to match_array(recipes.values_at(
+          1
+        ))
+      end
+    end
+
+    describe "case insensitivity" do
+      let(:search_ingredients) do
+        [
+          "OLIVE OIL",
+        ]
+      end
+
+      it "returns recipes with mistyped search ingredients" do
+        expect(searcher.recipes).to match_array(recipes.values_at(
+          1, 2
+        ))
+      end
+    end
+  end
+end
